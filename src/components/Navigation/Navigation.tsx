49441d59c6ab50dde5bd3d3cba42deb2db2b02d8
Yass <ynezreg@gmail.com>
2024-11-11 19:58:45 +0100

    feat: add logo as homepage button in Navigation bar with animation and styling

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -9,15 +9,21 @@ export const Navigation: React.FC = () => {
 
 
     return (
-        <nav className='navigation'>
-            <div className='navigation-links'>
-                <Link className='projects-list_link' to={'/projects'}>
-                    Projects
-                </Link>
-                <Link className='skills-list_link' to={'/Skills'}>
-                    Skills details
-                </Link>
-            </div>
-        </nav>
+    <>
+    <nav className='navigation'>
+        <Link className='navigation-logo' to={'/'}>
+                <h1>Yacine NEZREG</h1>
+                <p className='navigation-logo-description'>Front End Developer</p>
+        </Link>
+        <div className='navigation-links'>
+            <Link className='projects-list_link' to={'/projects'}>
+                Projects
+            </Link>
+            <Link className='skills-list_link' to={'/Skills'}>
+                Skills details
+            </Link>
+        </div>
+    </nav>
+    </>
     );
 };
