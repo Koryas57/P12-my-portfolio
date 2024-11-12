67bce4819a4baa600008fc4cb3e178318b38dd01
Yass <ynezreg@gmail.com>
2024-11-12 22:34:53 +0100

    Fix file import path for Showroom.scss

--- src/components/Showroom/Showroom.tsx
+++ src/components/Showroom/Showroom.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import './showroom.scss';
+import './Showroom.scss';
 
 interface ModelViewerModalProps {
   isOpen: boolean;
