459726ed95ea06b1f87ebe1ab5bb092a6a0c443f
Yass <ynezreg@gmail.com>
2024-11-13 14:43:21 +0100

    chore: enhance user experience and fine-tune project details

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,19 +9,15 @@ export const About: React.FC = () => {
       <img src={ProfilePic} alt="Photo de Yacine Nezreg"/>
       
       <p className="about-paragraph">
-        Depuis mon plus jeune âge, l’informatique et les nouvelles technologies ont toujours été au cœur de mes intérêts. Dès mes premières années au collège, je me suis lancé dans l'exploration des systèmes d'exploitation, en expérimentant dans le BIOS de Windows, le terminal CMD, et divers logiciels de l’époque. Cette curiosité autodidacte m’a rapidement conduit aux bases du développement en C et C++, un défi à la hauteur de ma soif de connaissances, bien que limité par mon âge à l'époque. Mon rêve de rejoindre une entreprise innovante comme Square Enix pour contribuer au développement de jeux vidéo et à la création d’animations 3D n’a cessé de guider mes ambitions.
+        Passioné depuis toujours par les technologies et l'informatique. Très jeune, je me suis interessé au fonctionnement des systèmes d'exploitations et explorais les bases de la programmation en C et C++ grâce aux cours sur le Site du Zero. Mon parcours m’a amené à travailler dans divers secteurs, notamment celui de la restauration, où j’ai découvert l'importance de la diversité culturelle et de la créativité. Aujourd'hui, je m’investis pleinement dans le développement web, motivé par la création d'expériences numériques interactives et captivantes.
       </p>
-      
+
       <p className="about-paragraph">
-        Avec le temps, ma vie d’adulte m'a conduit à explorer d'autres horizons. J'ai travaillé pendant dix ans en restauration, ce qui m'a permis de voyager à travers le monde, de découvrir de nouvelles cultures et d'acquérir une grande ouverture d’esprit. Ces expériences m’ont inspiré à poursuivre ce qui me passionne vraiment : le développement web et la création d'expériences interactives. Aujourd'hui, ce ne sont plus seulement les jeux vidéo qui m’intéressent, mais aussi la conception de sites web, de logiciels, et de fonctionnalités captivantes pour les utilisateurs. Je suis fasciné par les possibilités infinies qu'offrent les technologies modernes pour enrichir les expériences en ligne, que ce soit par des animations élégantes, des interfaces intuitives, ou des designs immersifs.
+        Cette année, j'ai poursuivi une formation d'intégrateur web chez OpenClassrooms, acquérant une solide maîtrise des technologies front-end, de l’intégration de maquettes aux animations avancées. Les projets que je présente témoignent de mon engagement envers l'innovation et la qualité.
       </p>
-      
-      <p className="about-paragraph">
-        Cette année, j'ai franchi une nouvelle étape en intégrant la formation d'intégrateur web chez OpenClassrooms, où j’ai pu affiner mes compétences et aller bien au-delà des bases. La formation m'a permis de maîtriser le développement front-end, l’intégration de maquettes, les animations et bien d’autres compétences indispensables pour devenir un développeur web complet. Les projets que je présente ici reflètent ce parcours et démontrent mon engagement à réaliser des solutions web créatives et fonctionnelles.
-      </p>
-      
+
       <p className="about-paragraph highlight">
-        Je vous remercie sincèrement de prendre le temps de parcourir mon portfolio. C’est un honneur de partager mon travail avec vous, et j'espère que nos chemins se croiseront pour collaborer sur des projets inspirants. Travailler avec des personnes passionnées et innovantes est un privilège, et je me réjouis de cette perspective.
+        Merci de parcourir mon portfolio. C'est un plaisir de partager mon parcours avec vous, et j’espère que nos chemins se croiseront pour collaborer sur des projets ambitieux avec des personnes passionnées et visionnaires.
       </p>
     </section>
   );
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './ContactForm.scss';
+import mailIcon from '../../assets/images/mail.png'
 
 export const ContactForm: React.FC = () => {
   return (
     <form className="contact-form">
-      <h2 className="contact-title">Contactez-moi</h2>
+      <div className='contact-header'>
+        <img src={mailIcon} alt="Icone d'enveloppe" />
+        <h2 className="contact-title">Contactez-moi</h2>
+      </div>
+
       <label htmlFor="name">Nom</label>
       <input type="text" id="name" name="name" required />
       
@@ -12,7 +17,7 @@ export const ContactForm: React.FC = () => {
       <input type="email" id="email" name="email" required />
       
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" rows={44} required></textarea>
+      <textarea id="message" name="message" rows={11} required></textarea>
       
       <button type="submit">Envoyer</button>
     </form>
