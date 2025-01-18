257e3a8b05b1b953c498c4c7ae04393b5a8e9573
Yass <ynezreg@gmail.com>
2025-01-18 23:54:16 +0100

    feat: configure Vite for external mobile testing on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permet l'accès depuis d'autres appareils
+    port: 5173,      // Définit le port (vous pouvez le changer si nécessaire)
+  },
 })
